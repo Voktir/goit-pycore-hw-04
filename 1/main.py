85a27350e65282c701f79e35e429de70886ab6a1
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-29 19:43:24 +0300

    second

--- 1/main.py
+++ 1/main.py
@@ -1,11 +1,11 @@
-def total_salary(path: str) -> tuple():
+def total_salary(path: str):
 
     total = 0
     str_count = 0
 
     with open(path, encoding="utf-8") as salary:
         for string in salary:
-            res = string.split(",")
+            res = string.strip().split(",")
             total += int(res[1])
             str_count += 1
 
---
+++ 2/main.py
@@ -0,0 +1,14 @@
+def get_cats_info(path):
+
+    lst = []
+
+    with open(path, encoding="utf-8", errors='replace') as cats:
+        for string in cats:
+            res = string.strip().split(",")
+            cats_dict = {"id": res[0], "name": res[1], "age": res[2]}
+            lst.append(cats_dict)
+
+    return lst
+
+cats_info = get_cats_info("2/cats_file.txt")
+print(cats_info)
