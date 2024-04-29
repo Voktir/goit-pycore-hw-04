f6cdb99968a0387a8a34857a7f82b9e4d431ca3b
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-29 17:50:26 +0300

    first

---
+++ 1/main.py
@@ -0,0 +1,17 @@
+def total_salary(path: str) -> tuple():
+
+    total = 0
+    str_count = 0
+
+    with open(path, encoding="utf-8") as salary:
+        for string in salary:
+            res = string.split(",")
+            total += int(res[1])
+            str_count += 1
+
+    ret = (total, int(total/str_count))
+    return ret
+
+
+total, average = total_salary("1/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
