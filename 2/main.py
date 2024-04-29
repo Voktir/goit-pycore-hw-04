2856dc961808c7ccf5c61b1e5a0a13475dc410a5
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-29 20:11:33 +0300

    second excertions

--- 2/main.py
+++ 2/main.py
@@ -5,7 +5,13 @@ def get_cats_info(path):
     with open(path, encoding="utf-8", errors='replace') as cats:
         for string in cats:
             res = string.strip().split(",")
-            cats_dict = {"id": res[0], "name": res[1], "age": res[2]}
+            try:
+                if len(res)<=3:
+                    cats_dict = {"id": res[0], "name": res[1], "age": res[2]}
+                else:
+                    continue
+            except:
+                continue
             lst.append(cats_dict)
 
     return lst
