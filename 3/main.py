23d4490abc71f6a7e4466003fe89e4da31b86569
Viktor Kozhemiaka <9075276@gmail.com>
2024-04-30 10:40:45 +0300

    third

---
+++ 3/main.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from colorama import Fore, Back, Style
+
+
+def p_elmnts(path):
+    
+    global temp_el
+    for el in path.iterdir():
+        s = f"{el}"
+        if s.find("\\") == -1 :
+            spliter = "/"
+        if s.find("/") == -1 :
+            spliter = "\\"
+        i = 0
+        s = ""
+        while (len(f"{el}".split(spliter)) - 1)*3 > i:
+            s = f" {s}"
+            i += 1 
+        if el.is_dir():
+            print(Fore.CYAN + f"{s}{el.relative_to(Path(el).parent)}\\" + Style.RESET_ALL)
+            p_elmnts(el)
+        else:            
+            print(Fore.GREEN + f"{s}{el.relative_to(Path(el).parent)}" + Style.RESET_ALL)
+            
+
+def main():
+    path_input = ""
+    while True:
+        print("")
+        print(Fore.BLUE + "Вітаю, цей застосунок приймає шлях до директорії в якості аргументу командного рядка\n і візуалізує структуру цієї директорії, виводячи імена всіх піддиректорій та файлів" + Style.RESET_ALL)            
+        print("")
+        path_choise = input(Fore.MAGENTA + "      Введіть 1 для демо режиму, чи 2 для вводу шляху вручну: " + Style.RESET_ALL)
+        match path_choise:
+            case "1":                
+                path_input = "3"
+                print(Fore.CYAN + f"{path_input}\\" + Style.RESET_ALL)
+                p_elmnts(Path(path_input))
+            case "2":
+                path_input = input(Fore.YELLOW + "Уважно введіть шлях вручну: " + Style.RESET_ALL)
+                print(Fore.CYAN + f"{Path(path_input)}\\" + Style.RESET_ALL)
+                try:
+                    p_elmnts(Path(path_input))
+                except:
+                    print (Fore.RED + "Помилка шляху. Уважно вводьте шлях!" + Style.RESET_ALL)
+            case _:
+                print (Fore.RED + "Зробіть правильний вибір: введіть 1 чи 2" + Style.RESET_ALL)
+
+
+if __name__ == "__main__":
+    main()
