16186c48abec8d90fc7def963bbcff26d094e8eb
Viktor Kozhemiaka <9075276@gmail.com>
2024-05-01 07:44:22 +0300

    Refinement

--- 1/main.py
+++ 1/main.py
@@ -3,14 +3,22 @@ def total_salary(path: str):
     total = 0
     str_count = 0
 
-    with open(path, encoding="utf-8") as salary:
-        for string in salary:
-            res = string.strip().split(",")
-            total += int(res[1])
-            str_count += 1
+    try:
 
-    ret = (total, int(total/str_count))
-    return ret
+        with open(path, encoding="utf-8") as salary:
+            for string in salary:
+                try:
+                    res = string.strip().split(",")
+                    total += int(res[1])
+                    str_count += 1
+                except:
+                    continue
+        
+        ret = (total, int(total/str_count))
+        return ret
+    
+    except FileNotFoundError as e:
+        print(f"FileNotFoundError: {e}")
 
 # total, average = total_salary("1/salary_file.txt")
 # print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
--- 2/main.py
+++ 2/main.py
@@ -2,19 +2,25 @@ def get_cats_info(path):
 
     lst = []
 
-    with open(path, encoding="utf-8", errors='replace') as cats:
-        for string in cats:
-            res = string.split(",")
-            try:
-                if len(res)<=3:
-                    cats_dict = {"id": res[0].strip(), "name": res[1].strip(), "age": res[2].strip()}
-                else:
-                    continue
-            except:
-                continue
-            lst.append(cats_dict)
+    try:
 
-    return lst
+        with open(path, encoding="utf-8", errors='replace') as cats:
+            
+            for string in cats:
+                res = string.split(",")            
+                try:
+                    if len(res)<=3:
+                        cats_dict = {"id": res[0].strip(), "name": res[1].strip(), "age": res[2].strip()}
+                    else:
+                        continue
+                except:
+                    continue
+                lst.append(cats_dict)
+        
+        return lst
+    
+    except FileNotFoundError as e:
+        print(f"FileNotFoundError: {e}")
 
 # cats_info = get_cats_info("2/cats_file.txt")
 # print(cats_info)
